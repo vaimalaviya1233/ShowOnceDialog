9aaaaa97a53b16fb07eba5301092bf5f1dd793f4
VaiMalaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2024-09-16 17:13:15 -0500

    added Material Dialog

--- app/src/main/java/dev/vaibhav/showoncedialog/Dialog_Fragment.java
+++ app/src/main/java/dev/vaibhav/showoncedialog/Dialog_Fragment.java
@@ -2,6 +2,7 @@ package dev.vaibhav.showoncedialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 public class Dialog_Fragment extends DialogFragment {
+	int dialogType = 1; // 1 for material dialog, 0 for custom dialog
 	String title; // title text of dialog
 	String body; // body text of dialog
 
@@ -21,18 +23,37 @@ public class Dialog_Fragment extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		SharedPreferences.Editor editor = requireActivity().getPreferences(Context.MODE_PRIVATE).edit();
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setTitle(title)
-				.setMessage(body) // sets body of dialog
-				.setPositiveButton(R.string.dialog_positive_button, (dialog, which) -> {
-					editor.putBoolean("dialog_accepted", true); // puts data-value in shared preferences
-					editor.apply();// executes operations to put data in shared preferences
-				})
-				.setNegativeButton(R.string.dialog_negative_button, (dialog, which) -> {
-					editor.putBoolean("dialog_accepted", false);
-					editor.apply();// executes operations to put data in shared preferences
-					requireActivity().finish();// closes fragment
-				});
-		return builder.create();
+
+		if(dialogType == 1) {
+			MaterialAlertDialogBuilder Mbuilder = new MaterialAlertDialogBuilder(getActivity());
+			Mbuilder.setTitle(title)
+					.setMessage(body)
+					.setPositiveButton(R.string.dialog_positive_button, (dialog, which) -> {
+						editor.putBoolean("dialog_accepted", true); // puts data-value in shared preferences
+						editor.apply();// executes operations to put data in shared preferences
+					})
+					.setNegativeButton(R.string.dialog_negative_button, (dialog, which) -> {
+						editor.putBoolean("dialog_accepted", false);
+						editor.apply();// executes operations to put data in shared preferences
+						requireActivity().finish();// closes fragment
+					});
+			return Mbuilder.create();
+		}else{
+			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+			builder.setTitle(title)
+					.setMessage(body) // sets body of dialog
+					.setPositiveButton(R.string.dialog_positive_button, (dialog, which) -> {
+						editor.putBoolean("dialog_accepted", true); // puts data-value in shared preferences
+						editor.apply();// executes operations to put data in shared preferences
+					})
+					.setNegativeButton(R.string.dialog_negative_button, (dialog, which) -> {
+						editor.putBoolean("dialog_accepted", false);
+						editor.apply();// executes operations to put data in shared preferences
+						requireActivity().finish();// closes fragment
+					});
+			return builder.create();
+		}
+
 	}
 }
